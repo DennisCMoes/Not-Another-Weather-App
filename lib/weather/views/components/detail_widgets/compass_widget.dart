c06f88ae30fccd25ba1c140f4cf97936394b8fe3
Dennis Moes <dennismoes@me.com>
2024-07-28 19:29:22 +0200

    Made some improvements on the compass design

--- lib/weather/views/components/detail_widgets/compass_widget.dart
+++ lib/weather/views/components/detail_widgets/compass_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:not_another_weather_app/weather/controllers/providers/current_geocoding_provider.dart';
+import 'package:not_another_weather_app/weather/models/colorscheme.dart';
 import 'package:not_another_weather_app/weather/models/widget_item.dart';
 import 'package:not_another_weather_app/weather/views/painters/compass_painter.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,28 @@ class CompassWidget extends StatelessWidget {
   final WidgetSize size;
 
   const CompassWidget({required this.size, super.key});
+
+  String getHeading([int heading = 0]) {
+    if (heading >= 0 && heading <= 22.5 && heading >= 337.5) {
+      return "N";
+    } else if (heading > 22.5 && heading <= 67.5) {
+      return "NE";
+    } else if (heading > 67.5 && heading <= 112.5) {
+      return "E";
+    } else if (heading > 112.5 && heading <= 157.5) {
+      return 'SE';
+    } else if (heading > 157.5 && heading <= 202.5) {
+      return 'S';
+    } else if (heading > 202.5 && heading <= 247.5) {
+      return 'SW';
+    } else if (heading > 247.5 && heading <= 292.5) {
+      return 'W';
+    } else if (heading > 292.5 && heading <= 337.5) {
+      return 'NW';
+    } else {
+      return 'XX';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,36 +68,64 @@ class CompassWidget extends StatelessWidget {
   }
 
   Widget _details(BuildContext context, CurrentGeocodingProvider provider) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-            "Wind speed: ${provider.geocoding.forecast?.windSpeed.round() ?? 0.0}"),
-        const Divider(),
-        Text(
-            "Wind gusts: ${provider.geocoding.forecast?.windGusts.round() ?? 0.0}"),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Text("Wind speed"),
+              Text(
+                  "${provider.geocoding.forecast?.windSpeed.round() ?? 0.0} km/h",
+                  style: Theme.of(context).textTheme.displayMedium),
+            ],
+          ),
+          Divider(
+            color: provider
+                    .geocoding.forecast?.weatherCode.colorScheme.accentColor ??
+                Colors.white,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                  "${provider.geocoding.forecast?.windGusts.round() ?? 0.0} km/h",
+                  style: Theme.of(context).textTheme.displayMedium),
+              const Text("Wind gusts")
+            ],
+          ),
+        ],
+      ),
     );
   }
 
   Widget _compass(BuildContext context, CurrentGeocodingProvider provider) {
     return CustomPaint(
       painter: CompassPainter(
-        angle: provider.geocoding.forecast?.windDirection.toDouble() ?? 0.0,
-        ringColor:
-            provider.geocoding.forecast?.weatherCode.colorScheme.accentColor ??
-                Colors.white,
+        direction: provider.geocoding.forecast?.windDirection.toDouble() ?? 0.0,
+        colorScheme: provider.geocoding.forecast?.weatherCode.colorScheme ??
+            WeatherColorScheme.gray,
       ),
-      child: Center(
-        child: Text(
-          "${provider.geocoding.forecast?.windDirection ?? "XX"}º",
-          style: Theme.of(context).textTheme.displayMedium!.copyWith(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Text("${provider.geocoding.forecast?.windDirection ?? "XX"}",
+          //     style: Theme.of(context).textTheme.displayMedium!.copyWith(
+          //         color: provider.geocoding.forecast?.weatherCode.colorScheme
+          //                 .accentColor ??
+          //             Colors.white)),
+          Text(
+            getHeading(provider.geocoding.forecast?.windDirection ?? 0),
+            style: Theme.of(context).textTheme.displayMedium!.copyWith(
                 color: provider.geocoding.forecast?.weatherCode.colorScheme
                         .accentColor ??
-                    Colors.white,
-              ),
-        ),
+                    Colors.white),
+          ),
+        ],
       ),
     );
   }
--- lib/weather/views/painters/compass_painter.dart
+++ lib/weather/views/painters/compass_painter.dart
@@ -1,56 +1,117 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:not_another_weather_app/weather/models/colorscheme.dart';
 
+/// A custom painter for drawing a compass with a needle indicating a direction
 class CompassPainter extends CustomPainter {
-  final double angle;
-  final Color ringColor;
+  final double direction;
+  final WeatherColorScheme colorScheme;
 
-  CompassPainter({required this.angle, required this.ringColor});
+  CompassPainter({required this.direction, required this.colorScheme});
 
-  double get rotation => -2 * pi * (angle / 360);
-
-  Paint get _brush => Paint()
-    ..style = PaintingStyle.stroke
-    ..strokeWidth = 2.0;
+  double get rotation => -2 * pi * (direction / 360);
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint circle = _brush..color = Colors.indigo;
-    Paint needle = _brush..color = Colors.red;
+    final Paint circlePaint = Paint()
+      ..color = colorScheme.accentColor.withOpacity(0.4)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    final Paint needlePaint = Paint()
+      ..color = colorScheme.accentColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    final Paint endCirclePaint = Paint()
+      ..color = colorScheme.darkenMainColor(0.07)
+      ..style = PaintingStyle.fill;
+
+    final Paint tickPaint = Paint()
+      ..color = colorScheme.accentColor.withOpacity(0.4)
+      ..strokeWidth = 1.5;
+
+    final Paint needleCirclePaint = Paint()
+      ..color = colorScheme.accentColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0;
+
+    final Paint needleArrowheadPaint = Paint()
+      ..color = colorScheme.accentColor
+      ..style = PaintingStyle.fill;
 
     double radius = min(size.width / 2.2, size.height / 2.2);
     Offset center = Offset(size.width / 2, size.height / 2);
-    Offset start = Offset.lerp(Offset(center.dx, radius), center, 10)!;
-    Offset end = Offset.lerp(Offset(center.dx, radius), center, 6)!;
 
     // Draw the compass circle
-    canvas.drawCircle(center, radius, circle);
+    canvas.drawCircle(center, radius, circlePaint);
 
-    // Rotate the canvas for the needle
-    drawCardinalDirection(canvas, center, 'N', Offset(0, -radius + 15));
-    drawCardinalDirection(canvas, center, 'E', Offset(radius - 15, 0));
-    drawCardinalDirection(canvas, center, 'S', Offset(0, radius - 15));
-    drawCardinalDirection(canvas, center, 'W', Offset(-radius + 15, 0));
+    // Draw ticks for every 30 degrees and shorter ticks for every 10 degrees
+    for (double angle = 0; angle < 360; angle += 10) {
+      final double tickLength = angle % 30 == 0 ? 10 : 5;
+      final Offset startTick =
+          _calculatePoint(center, radius - tickLength, angle);
+      final Offset endTick = _calculatePoint(center, radius, angle);
 
-    // Rotate the canvas for the needle
-    canvas.save();
-    canvas.translate(center.dx, center.dy);
-    canvas.rotate(rotation);
-    canvas.translate(-center.dx, -center.dy);
+      canvas.drawLine(startTick, endTick, tickPaint);
+    }
+
+    // Draw cardinal directions
+    _drawCardinalDirection(canvas, center, 'N', Offset(0, -radius + 20));
+    _drawCardinalDirection(canvas, center, 'E', Offset(radius - 20, 0));
+    _drawCardinalDirection(canvas, center, 'S', Offset(0, radius - 20));
+    _drawCardinalDirection(canvas, center, 'W', Offset(-radius + 20, 0));
 
     // Draw the needle
-    canvas.drawLine(start, end, needle);
-    canvas.restore();
+    final Offset needleStart =
+        _calculatePoint(center, radius - 12, direction + 180);
+    final Offset needleEnd = _calculatePoint(center, radius - 22, direction);
+    canvas.drawLine(needleStart, needleEnd, needlePaint);
+
+    // Draw the end circle
+    const double endCircleRadius = 7;
+    final Offset endCircleCenter =
+        _calculatePoint(center, radius - endCircleRadius - 10, direction);
+    canvas.drawCircle(endCircleCenter, endCircleRadius, needleCirclePaint);
+
+    // Draw the arrowhead
+    final Offset arrowheadPoint =
+        _calculatePoint(center, radius - 10, direction + 180);
+    canvas.drawPath(_calculateArrowHeadPath(arrowheadPoint, direction),
+        needleArrowheadPaint);
+
+    canvas.drawCircle(center, 40, endCirclePaint);
   }
 
-  void drawCardinalDirection(
+  /// Calculates a point on the circumference of a circle
+  ///
+  /// [center] is the center of the circle
+  /// [radius] is the radius of the circle
+  /// [angle] is the angle in degrees from the positive x-axis
+  Offset _calculatePoint(Offset center, double radius, double angle) {
+    final adjustedDirection = angle - 90;
+    final radians = adjustedDirection * (pi / 180);
+
+    final xCor = center.dx + radius * cos(radians);
+    final yCor = center.dy + radius * sin(radians);
+
+    return Offset(xCor, yCor);
+  }
+
+  /// Draws a cardinal direction label on the canvas
+  ///
+  /// [canvas] is the canvas to draw on
+  /// [center] is the center of the compass
+  /// [label] is the direction label (e.g., 'N', 'E', 'S', 'W')
+  /// [offset] is the position offset for the label
+  void _drawCardinalDirection(
       Canvas canvas, Offset center, String direction, Offset offset) {
     final textPainter = TextPainter(
       text: TextSpan(
         text: direction,
-        style: const TextStyle(
-          color: Colors.black,
+        style: TextStyle(
+          color: colorScheme.accentColor.withOpacity(0.6),
           fontSize: 20,
           fontWeight: FontWeight.bold,
         ),
@@ -62,6 +123,38 @@ class CompassPainter extends CustomPainter {
     Offset position =
         center + offset - Offset(textPainter.width / 2, textPainter.height / 2);
     textPainter.paint(canvas, position);
+  }
+
+  /// Draws an arrowhead at a given point on the canvas
+  ///
+  /// [canvas] is the canvas to draw on
+  /// [point] is the point where the arrowhead is drawn
+  /// [angle] is the direction angle of the arrowhead in degrees
+  Path _calculateArrowHeadPath(Offset point, double angle) {
+    const arrowSize = 15.0;
+    const arrowAngle = pi / 6; // 30 degrees
+
+    final path = Path();
+    final adjustedDirection = angle - 90;
+    final radians = adjustedDirection * (pi / 180);
+
+    // Calculate the points for the arrowhead triangle
+    final leftPoint = Offset(
+      point.dx + arrowSize * cos(radians + arrowAngle),
+      point.dy + arrowSize * sin(radians + arrowAngle),
+    );
+
+    final rightPoint = Offset(
+      point.dx + arrowSize * cos(radians - arrowAngle),
+      point.dy + arrowSize * sin(radians - arrowAngle),
+    );
+
+    path.moveTo(point.dx, point.dy);
+    path.lineTo(leftPoint.dx, leftPoint.dy);
+    path.lineTo(rightPoint.dx, rightPoint.dy);
+    path.close();
+
+    return path;
   }
 
   @override
