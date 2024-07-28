38b493b0eeb91c392a435ca2f9f1487e12563efb
Dennis Moes <dennismoes@me.com>
2024-07-28 10:34:40 +0200

    Added documentation comments

--- lib/weather/controllers/providers/current_geocoding_provider.dart
+++ lib/weather/controllers/providers/current_geocoding_provider.dart
@@ -4,6 +4,7 @@ import 'package:not_another_weather_app/weather/models/forecast.dart';
 import 'package:not_another_weather_app/weather/models/geocoding.dart';
 import 'package:not_another_weather_app/weather/models/widget_item.dart';
 
+/// A provider that manages the state and logic for the currently selected geocoding
 class CurrentGeocodingProvider extends ChangeNotifier {
   final Geocoding geocoding;
   final PageController _pageController = PageController(initialPage: 0);
@@ -17,23 +18,30 @@ class CurrentGeocodingProvider extends ChangeNotifier {
   int get subPageIndex => _subPageIndex;
   PageController get subPageController => _pageController;
 
+  /// Checks if the given [index] is the current page.
+  ///
+  /// Returns `true` if the [index] matches the current sub-page index
   bool isCurrentPage(int index) => _subPageIndex == index;
 
+  /// Sets the editing state to [isEditing] and notifies it's listeners
   void setIsEditing(bool isEditing) {
     _isEditing = isEditing;
     notifyListeners();
   }
 
+  /// Sets the sub-page index to [index] and notifies it's listeners
   void setSubPageIndex(int index) {
     _subPageIndex = index;
     notifyListeners();
   }
 
+  /// Sets the main field of the geocoding to [field] and notifies it's listeners
   void setMainField(SelectableForecastFields field) {
     geocoding.selectedMainField = field;
     notifyListeners();
   }
 
+  /// Replaces the [oldField] with [newField] in the list of selected forecast items and notifies it's listeners
   void replaceSecondaryField(
       SelectableForecastFields oldField, SelectableForecastFields newField) {
     int index = geocoding.selectedForecastItems.indexOf(oldField);
@@ -41,12 +49,14 @@ class CurrentGeocodingProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Sets the size of the widget item identified by [item] to [size] and notifies it's listeners
   void setGeocodingSize(WidgetItem item, WidgetSize size) {
     geocoding.detailWidgets.singleWhere((widget) => widget.id == item.id).size =
         size;
     notifyListeners();
   }
 
+  /// Sets the type of the widget item identified by [item] to [type] and notifies it's listeners
   void setGeocodingType(WidgetItem item, WidgetType type) {
     geocoding.detailWidgets.singleWhere((widget) => widget.id == item.id).type =
         type;
--- lib/weather/controllers/providers/weather_provider.dart
+++ lib/weather/controllers/providers/weather_provider.dart
@@ -1,13 +1,12 @@
 import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:not_another_weather_app/shared/utilities/controllers/location_controller.dart';
 import 'package:not_another_weather_app/weather/controllers/repositories/forecast_repo.dart';
 import 'package:not_another_weather_app/weather/models/forecast.dart';
 import 'package:not_another_weather_app/weather/models/geocoding.dart';
-import 'package:not_another_weather_app/weather/models/widget_item.dart';
 
+/// A provider class that manages the weather-related data and operations
 class WeatherProvider extends ChangeNotifier {
   final ForecastRepo _forecastRepo = ForecastRepo();
   final LocationController _locationController = LocationController();
@@ -22,85 +21,52 @@ class WeatherProvider extends ChangeNotifier {
   UnmodifiableListView<Geocoding> get geocodings =>
       UnmodifiableListView(_geocodings);
 
+  /// Initializes the weather provider by fetching the current location and it's forecasts, and notifying it's listeners.
   Future<void> initialization() async {
     try {
       final Position position = await _locationController.getCurrentPosition();
-      _geocodings.add(Geocoding(1, "My location", position.latitude,
-          position.longitude, 'My location',
-          isCurrentLocation: true));
+      final currentLocationGeocoding = Geocoding(1, "My location",
+          position.latitude, position.longitude, "My location",
+          isCurrentLocation: true);
+
+      _geocodings.add(currentLocationGeocoding);
       notifyListeners();
 
-      _geocodings[0].forecast = await _forecastRepo.getLocalForecast(
+      currentLocationGeocoding.forecast = await _forecastRepo.getLocalForecast(
           position.latitude, position.longitude);
       notifyListeners();
-
-      // await getStoredGeocodings();
     } catch (ex) {
       debugPrint("Error during initialization: $ex");
     }
   }
 
+  /// Retrieves the stored geocodings and their forecasts
   Future<void> getStoredGeocodings() async {
     try {
-      final updatedGeocodings =
-          await Future.wait(Geocoding.sampleData().map((geo) async {
-        final forecast =
-            await _forecastRepo.getLocalForecast(geo.latitude, geo.longitude);
+      final List<Geocoding> storedGeocodings = await Future.wait(
+        Geocoding.sampleData().map((geo) async {
+          final forecast =
+              await _forecastRepo.getLocalForecast(geo.latitude, geo.longitude);
+          geo.forecast = forecast;
+          return geo;
+        }).toList(),
+      );
 
-        geo.forecast = forecast;
-        return geo;
-      }).toList());
-
-      _geocodings.addAll(updatedGeocodings);
-
+      _geocodings.addAll(storedGeocodings);
       notifyListeners();
     } catch (ex) {
       debugPrint("Error getting the stored geocodings: $ex");
     }
   }
 
+  // Moves a geocoding location from [oldIndex] to [newIndex] in the list
   void moveGeocodings(int oldIndex, int newIndex) {
     final item = _geocodings.removeAt(oldIndex);
     _geocodings.insert(newIndex, item);
-
     notifyListeners();
   }
 
-  void changeSelectedMainField(
-      Geocoding selectedGeocoding, SelectableForecastFields field) {
-    int index = _geocodings.indexOf(selectedGeocoding);
-
-    if (index != -1) {
-      _geocodings[index].selectedMainField = field;
-      notifyListeners();
-    } else {
-      debugPrint("Error: Geocoding not found.");
-    }
-  }
-
-  void replaceSelectedForecastItem(
-      Geocoding selectedGeocoding, SelectableForecastFields field, int index) {
-    int geoIndex = _geocodings.indexOf(selectedGeocoding);
-
-    if (geoIndex != -1 && index != -1) {
-      _geocodings[geoIndex].selectedForecastItems[index] = field;
-      notifyListeners();
-    } else {
-      debugPrint("Error: Geocoding not found.");
-    }
-  }
-
-  void changeSelectedPage(Geocoding selectedGeocoding, int index) {
-    int geoIndex = _geocodings.indexOf(selectedGeocoding);
-
-    if (geoIndex != -1 && index != -1) {
-      _geocodings[geoIndex].selectedPage = index;
-      notifyListeners();
-    } else {
-      debugPrint("Error: Geocoding not found.");
-    }
-  }
-
+  /// Adds a new geocoding location and fetches it's forecast
   Future<void> addGeocoding(Geocoding geocoding) async {
     _geocodings.add(geocoding);
     notifyListeners();
@@ -113,43 +79,12 @@ class WeatherProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Removes the specified [geocoding] location from the list
   void removeGeocoding(Geocoding geocoding) {
     int geoIndex = _geocodings.indexOf(geocoding);
 
     if (geoIndex != -1) {
       _geocodings.removeAt(geoIndex);
-      notifyListeners();
-    } else {
-      debugPrint("Error: Geocoding not found");
-    }
-  }
-
-  void changeGeocodingSize(
-      Geocoding geocoding, WidgetItem item, WidgetSize size) {
-    int geoIndex = _geocodings.indexOf(geocoding);
-
-    if (geoIndex != -1) {
-      _geocodings[geoIndex]
-          .detailWidgets
-          .singleWhere((element) => element.id == item.id)
-          .size = size;
-
-      notifyListeners();
-    } else {
-      debugPrint("Error: Geocoding not found");
-    }
-  }
-
-  void changeGeocodingType(
-      Geocoding geocoding, WidgetItem item, WidgetType type) {
-    int geoIndex = _geocodings.indexOf(geocoding);
-
-    if (geoIndex != -1) {
-      _geocodings[geoIndex]
-          .detailWidgets
-          .singleWhere((element) => element.id == item.id)
-          .type = type;
-
       notifyListeners();
     } else {
       debugPrint("Error: Geocoding not found");
