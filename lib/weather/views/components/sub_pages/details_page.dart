05ae0d565b06facd91714e63f9ccca7be983c132
Dennis Moes <dennismoes@me.com>
2024-07-28 19:30:36 +0200

    Moved some logics and removed redundant code

--- lib/weather/views/components/sub_pages/details_page.dart
+++ lib/weather/views/components/sub_pages/details_page.dart
@@ -43,163 +43,52 @@ class _PageTwoState extends State<PageTwo> {
       );
     }
 
-    return Consumer<CurrentGeocodingProvider>(builder: (context, state, child) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: NavigationToolbar.kMiddleSpacing),
-        child: StaggeredGrid.count(
-          axisDirection: AxisDirection.down,
-          crossAxisCount: 4,
-          crossAxisSpacing: 6,
-          mainAxisSpacing: 6,
-          children: [
-            for (int i = 0; i < state.geocoding.detailWidgets.length; i++)
-              StaggeredGridTile.count(
-                crossAxisCellCount:
-                    state.geocoding.detailWidgets[i].size.colSpan,
-                mainAxisCellCount:
-                    state.geocoding.detailWidgets[i].size.rowSpan,
-                child: Material(
-                  color: Colors.blue[900],
-                  borderRadius: BorderRadius.circular(12),
-                  clipBehavior: Clip.hardEdge,
-                  child: InkWell(
-                    splashColor: state.isEditing ? null : Colors.transparent,
-                    highlightColor: state.isEditing ? null : Colors.transparent,
-                    onTap: () {
-                      if (state.isEditing) {
-                        openWidgetDetail(state.geocoding.detailWidgets[i]);
-                      }
-                    },
-                    child: state.geocoding.detailWidgets[i]
-                        .getWidget(state.geocoding),
+    return Consumer<CurrentGeocodingProvider>(
+      builder: (context, state, child) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: NavigationToolbar.kMiddleSpacing, vertical: 6),
+          child: StaggeredGrid.count(
+            axisDirection: AxisDirection.down,
+            crossAxisCount: 4,
+            crossAxisSpacing: 6,
+            mainAxisSpacing: 6,
+            children: [
+              for (int i = 0; i < state.geocoding.detailWidgets.length; i++)
+                StaggeredGridTile.count(
+                  crossAxisCellCount:
+                      state.geocoding.detailWidgets[i].size.colSpan,
+                  mainAxisCellCount:
+                      state.geocoding.detailWidgets[i].size.rowSpan,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        width: 2,
+                        color: state.geocoding.forecast?.weatherCode.colorScheme
+                                .darkenMainColor(0.1) ??
+                            Colors.white,
+                      ),
+                    ),
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: state.isEditing ? null : Colors.transparent,
+                      highlightColor:
+                          state.isEditing ? null : Colors.transparent,
+                      onTap: () {
+                        if (state.isEditing) {
+                          openWidgetDetail(state.geocoding.detailWidgets[i]);
+                        }
+                      },
+                      child: state.geocoding.detailWidgets[i]
+                          .getWidget(state.geocoding),
+                    ),
                   ),
                 ),
-              ),
-          ],
-        ),
-      );
-    });
+            ],
+          ),
+        );
+      },
+    );
   }
-
-  // Widget _forecastCardWrapper({required String value, required Widget child}) {
-  //   return Material(
-  //     borderRadius: BorderRadius.circular(6),
-  //     color: widget.geocoding.forecast?.weatherCode.colorScheme
-  //             .darkenMainColor(0.1) ??
-  //         Colors.blueGrey,
-  //     clipBehavior: Clip.hardEdge,
-  //     child: Stack(
-  //       children: [
-  //         Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.0),
-  //           child: Align(
-  //             alignment: Alignment.topLeft,
-  //             child: Text(value),
-  //           ),
-  //         ),
-  //         Center(child: child),
-  //       ],
-  //     ),
-  //   );
-  // }
-
-  // Widget _feelAndSightCard(String value, IconData icon) {
-  //   return Row(
-  //     crossAxisAlignment: CrossAxisAlignment.center,
-  //     mainAxisAlignment: MainAxisAlignment.center,
-  //     children: [
-  //       Text(value, style: Theme.of(context).textTheme.displayLarge),
-  //       // const SizedBox(width: 4),
-  //       // Icon(icon, size: 32),
-  //     ],
-  //   );
-  // }
-
-  // Widget _compassCard() {
-  //   return Material(
-  //     color: widget.geocoding.forecast?.weatherCode.colorScheme
-  //             .darkenMainColor(0.1) ??
-  //         Colors.blueGrey,
-  //     borderRadius: BorderRadius.circular(8),
-  //     child: CustomPaint(
-  //       foregroundPainter: CompassPainter(
-  //         angle: widget.geocoding.forecast?.windDirection.toDouble() ?? 0.0,
-  //         ringColor:
-  //             widget.geocoding.forecast?.weatherCode.colorScheme.accentColor ??
-  //                 Colors.black,
-  //       ),
-  //       child: Center(
-  //         child: Text(
-  //           "${widget.geocoding.forecast?.windDirection ?? "XX"}º",
-  //           style: Theme.of(context).textTheme.displayMedium,
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // Widget _rainForecastCard() {
-  //   return CustomPaint(
-  //     painter:
-  //         LineChartPainter(widget.geocoding.forecast?.rainProbabilities ?? {}),
-  //     child: Container(),
-  //   );
-  // }
-
-  // Widget _isDayCard() {
-  //   String formatTime(DateTime? dateTime) {
-  //     return dateTime == null
-  //         ? "Invalid time"
-  //         : DateFormat.Hm().format(dateTime);
-  //   }
-
-  //   return Padding(
-  //     padding: const EdgeInsets.all(8.0),
-  //     child: Stack(
-  //       fit: StackFit.expand,
-  //       children: [
-  //         Align(
-  //           alignment: Alignment.topRight,
-  //           child: Column(
-  //             mainAxisSize: MainAxisSize.min,
-  //             crossAxisAlignment: CrossAxisAlignment.end,
-  //             children: [
-  //               const Text("Sunrise"),
-  //               Text(
-  //                 formatTime(widget.geocoding.forecast?.sunrise),
-  //                 style: Theme.of(context).textTheme.displaySmall,
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //         Align(
-  //           alignment: Alignment.bottomLeft,
-  //           child: Column(
-  //             mainAxisSize: MainAxisSize.min,
-  //             crossAxisAlignment: CrossAxisAlignment.start,
-  //             children: [
-  //               const Text("Sunset"),
-  //               Text(
-  //                 formatTime(widget.geocoding.forecast?.sunset),
-  //                 style: Theme.of(context).textTheme.displaySmall,
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //         CustomPaint(
-  //           painter: SunPainter(currentTime: DateTime.now()),
-  //           child: Container(),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
-  // Widget _pressureCard() {
-  //   return CustomPaint(
-  //     painter: PressureGaugePainter(pressure: 1015),
-  //     child: Container(),
-  //   );
-  // }
 }
