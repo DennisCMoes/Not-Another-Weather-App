574b750f25bc67c72541f862728d9eb5fd8ecc4b
Dennis Moes <dennismoes@me.com>
2024-08-23 06:45:57 +0200

    Added Tabler Icons and changed some icons in the selectable widgets

--- lib/weather/models/logics/selectable_forecast_fields.dart
+++ lib/weather/models/logics/selectable_forecast_fields.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
 enum SelectableForecastFields {
   temperature("Temperature", Icons.thermostat),
-  apparentTemperature("Apparent temperature", Icons.person),
-  windSpeed("Wind speed", Icons.air),
-  precipitation("Precipitation", Icons.water_drop),
+  apparentTemperature("Apparent temperature", TablerIcons.user),
+  windSpeed("Wind speed", TablerIcons.wind),
+  precipitation("Precipitation", Icons.water_drop_outlined),
   chainceOfRain("Chance of rain", Icons.umbrella),
-  sunrise("Sunrise", Icons.keyboard_arrow_up),
-  sunset("Sunset", Icons.keyboard_arrow_down),
+  sunrise("Sunrise", TablerIcons.sunrise),
+  sunset("Sunset", TablerIcons.sunset),
   humidity("Humidity", Icons.percent),
-  windDirection("Wind direction", Icons.directions),
+  windDirection("Wind direction", TablerIcons.compass),
   windGusts("Wind gusts", Icons.speed),
-  cloudCover("Cloud cover", Icons.cloud),
-  localTime("Local time", Icons.circle);
+  cloudCover("Cloud cover", TablerIcons.cloud),
+  localTime("Local time", TablerIcons.clock);
 
   const SelectableForecastFields(this.label, this.icon);
 
