ce8fa084bdd3a234aec0d703b2731c897bd2ebcd
Dennis Moes <dennismoes@me.com>
2024-08-24 08:50:57 +0200

    Changed the outlined to filled icons

--- lib/weather/models/logics/selectable_forecast_fields.dart
+++ lib/weather/models/logics/selectable_forecast_fields.dart
@@ -3,16 +3,16 @@ import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
 enum SelectableForecastFields {
   temperature("Temperature", Icons.thermostat),
-  apparentTemperature("Apparent temperature", TablerIcons.user),
+  apparentTemperature("Apparent temperature", TablerIcons.user_filled),
   windSpeed("Wind speed", TablerIcons.wind),
-  precipitation("Precipitation", Icons.water_drop_outlined),
+  precipitation("Precipitation", Icons.water_drop),
   chainceOfRain("Chance of rain", Icons.umbrella),
   sunrise("Sunrise", TablerIcons.sunrise),
   sunset("Sunset", TablerIcons.sunset),
   humidity("Humidity", Icons.percent),
   windDirection("Wind direction", TablerIcons.compass),
   windGusts("Wind gusts", Icons.speed),
-  cloudCover("Cloud cover", TablerIcons.cloud),
+  cloudCover("Cloud cover", TablerIcons.cloud_filled),
   localTime("Local time", TablerIcons.clock);
 
   const SelectableForecastFields(this.label, this.icon);
